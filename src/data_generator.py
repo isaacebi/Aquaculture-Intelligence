565aaec09f255c6d3fea92c67c88ad0e31152a0a
isaacebi <isaac_ebi@rocketmail.com>
2024-07-26 11:27:06 +0800

    Update data_generator.py
    Change the picture file name to contain abnormal level, experiment and frame number

--- src/data_generator.py
+++ src/data_generator.py
@@ -67,7 +67,7 @@ class PictureGenerator:
         return df['random_time']
 
 
-    def take_snapshot(self, video_path, condition_level, time_seconds=0):
+    def take_snapshot(self, video_path, file_name, time_seconds=0):
         """
         Captures a snapshot from a video file or camera stream at a specified time.
         
@@ -107,7 +107,7 @@ class PictureGenerator:
         # Save the frame as an image
         if not os.path.exists(self.save_path):
             os.makedirs(self.save_path)
-        output_path = os.path.join(self.save_path, f"{condition_level}_{frame_number}.png")
+        output_path = os.path.join(self.save_path, f"{file_name}_{frame_number}.png")
         cv2.imwrite(output_path, frame)
         
         # Release the video capture object
@@ -118,8 +118,10 @@ class PictureGenerator:
     
 def main():
     # Hard coded path
-    DATA_FOLDER = GetPath().data()
-    RANDOM_TIME_FOLDER = os.path.join(os.getcwd(), 'data', 'random_time')
+    DATA_FOLDER = GetPath().shared_data()
+    LOCAL_DATA_FOLDER = GetPath().local_data()
+
+    RANDOM_TIME_FOLDER = os.path.join(LOCAL_DATA_FOLDER, 'data', 'random_time')
     ABN_B1_TIME = os.path.join(RANDOM_TIME_FOLDER, 'abnormal_b1.csv')
     ABN_B2_TIME = os.path.join(RANDOM_TIME_FOLDER, 'abnormal_b2.csv')
     ABN_B3_TIME = os.path.join(RANDOM_TIME_FOLDER, 'abnormal_b3.csv')
@@ -131,35 +133,20 @@ def main():
     #
     pic_gen = PictureGenerator(save_path=OUTPUT_PATH)
 
-    df = pd.DataFrame()
-
     for i in range(3):
         exp_name = f"ABN_B{i+1}"
-        dfTemp = pd.read_csv(eval(f"ABN_B{i+1}_TIME"))
-        dfTemp['experiment'] = exp_name
-        dfTemp['random_time'] = pic_gen.time_to_snapshot(
-            df=dfTemp
+        df = pd.read_csv(eval(f"ABN_B{i+1}_TIME"))
+        df['experiment'] = exp_name
+        df['random_time'] = pic_gen.time_to_snapshot(
+            df=df
         )
 
         vid_path = abn_vids[i]
-        dfTemp['random_seconds'].apply(lambda x: pic_gen.take_snapshot(
-            video_path=vid_path,
-            condition_level=exp_name,
-            time_seconds=int(x)
-        ))
-
-        df = pd.concat([df, dfTemp])
-
-
-    # History
-    file_uuid = str(uuid.uuid4())
-    rt_folder = os.path.join(DATA_FOLDER, 'random_time')
-    if not os.path.exists(rt_folder):
-        os.makedirs(rt_folder)
-    rt_csv = os.path.join(rt_folder, f'randomTime_{file_uuid}.csv')
-    df.to_csv(rt_csv)
-
-    return df
+        df.apply(lambda x: pic_gen.take_snapshot(
+            video_path = vid_path,
+            file_name=f"{x['experiment']}_{x['ABN']}",
+            time_seconds=int(x['random_seconds'])
+        ), axis=1)
 
 if __name__ == "__main__":
     main()
